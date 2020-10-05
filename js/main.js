fd771176b5698ea35ea7905105cc522afafbf7be
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-10-05 23:18:15 +0300

    FEAT: slider preperation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function($) {
+	//main form title
 	$('.hero-form__title').val ($('.hero-form__title').attr('data-main'));
+	//swiper
+	var swiper = new Swiper('.swiper-container', {
+		direction: 'vertical',
+        slidesPerView: 1,
+      	scrollbar: {
+        	el: '.swiper-scrollbar',
+        	hide: false,
+        	draggable: true,
+      },
+    });
 });
